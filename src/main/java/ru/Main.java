f700ef49ca5e19c9cbb1ad109360b83f85f2e501
Acer <alinakowaltschuk@mail.ru>
2025-02-24 22:34:26 +1000

    1.5

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -1,7 +1,10 @@
 package ru;
 
+import ru.calculator.Calculator;
+
 public class Main {
     public static void main(String[] args) {
+        Calculator.plus(1, 30);
         System.out.println("Hello world");
     }
 }
--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -8,9 +8,15 @@ public class Calculator {
 
     }
 
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        Calculator.plus(100, 500);
-        Calculator.plus(4, 2);
-        Calculator.plus(3, 5);
+        plus(100, 500);
+        plus(4, 2);
+        plus(3, 5);
+        minus(4, 6);
     }
 }
